84255e998d8d58633340b4c33b006043de8fe150
Phan Gia Anh <phangiaanhlop99@gmail.com>
2021-01-02 22:05:36 +0700

    Update image and login

--- script/script.js
+++ script/script.js
@@ -2,6 +2,9 @@
 var temp;
 var Enterprise;
 
+function closeSpan() {
+    document.getElementById("myModal").style.display = "none";
+}
 
 
 function listAllEnterprises() {
@@ -26,15 +29,35 @@ function listAllEnterprises() {
                 let item = document.createElement('div')
                 item.className = "grid-item"
                 let text = document.createElement('h2')
+                let img = document.createElement('IMG')
+                img.setAttribute("src", temp[i].Enterprise_Avatar)
+                img.setAttribute("width", 200)
+                img.setAttribute("height", 150)
                 text.innerHTML = temp[i].Enterprise_Name
                 item.appendChild(text)
+                item.appendChild(img)
                 button.appendChild(item)
                 document.getElementById("Enterprises Framework").appendChild(button)
             
                 button.onclick = () => {
+                    document.getElementById("myModal").style.display = "block";
                     Enterprise = text.innerHTML
                 }
             }
         }    
     })
+}
+
+function checkForm(e, text) {
+    var code = (e.keyCode ? e.keyCode : e.which)
+    if (code == 13) {
+        // $.ajax({
+        //     url: "localhost:9999/account",
+        //     dataType: 'jsonp',
+        //     data: {
+        //         user: document.getElementById("user").node
+        //     }
+        // })
+        console.log(document.getElementById("user").value)
+    }
 }
