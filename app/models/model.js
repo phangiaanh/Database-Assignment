4bf231d2e669b45d4cb0f9dd67174cfff0455f92
Phan Gia Anh <phangiaanhlop99@gmail.com>
2021-01-04 14:47:50 +0700

    Add Long

--- app/models/model.js
+++ app/models/model.js
@@ -84,3 +84,74 @@ exports.deleteEnterpriseWithID = function(id, res) {
         res.send(results)
     })
 }
+
+
+
+exports.getEntNotInVL_Has_lt4_store = function(req, res) {
+    var sql = sqlCmd.getEntNotInVL_Has_lt4_store;
+    conn.conn.query(sql, (err, results) => {
+        if (err) throw err;
+        //console.log(results)
+        res.send(results)
+    })
+}
+
+exports.getExactStoreAddress = function(req, res) {
+    var sql = sqlCmd.getExactStoreAddress;
+    conn.conn.query(sql, (err, results) => {
+        if (err) throw err;
+        //console.log(results[0])
+        res.send(results)
+    })
+}
+exports.getAllStoreByEntID = function(id, res) {
+    var sql = sqlCmd.getAllStoreByEntID(id);
+    conn.conn.query(sql, (err, results) => {
+        if (err) throw err;
+        //console.log(results[0])
+        res.send(results)
+    })
+}
+exports.getAllStoreAddressExcept = function(id, res) {
+    var sql = sqlCmd.getAllStoreAddressExcept(id);
+    conn.conn.query(sql, (err, results) => {
+        if (err) throw err;
+        //console.log(results[0])
+        res.send(results)
+    })
+}
+exports.getStoreCountByID = function(id, res) {
+    var sql = sqlCmd.getNumberOfStoreByID(id);
+    conn.conn.query(sql, (err, results) => {
+        if (err) throw err;
+        //console.log(results[0])
+        res.send(results[0])
+    })
+}
+
+exports.getAllStock = function(req, res) {
+    var sql = sqlCmd.getAllStock;
+    conn.conn.query(sql, (err, results) => {
+        if (err) throw err;
+        //console.log(results[0])
+        res.send(results)
+    })
+}
+
+exports.getStockSupporting_mt2_Store = function(req, res) {
+    var sql = sqlCmd.getStockSupporting_mt2_Store;
+    conn.conn.query(sql, (err, results) => {
+        if (err) throw err;
+        //console.log(results[0])
+        res.send(results)
+    })
+}
+
+exports.getStockAreaLessThan = function(square, res) {
+    var sql = sqlCmd.getStockAreaLessThan(square);
+    conn.conn.query(sql, (err, results) => {
+        if (err) throw err;
+        //console.log(results)
+        res.send(results)
+    })
+}
--- app/routes/routes.js
+++ app/routes/routes.js
@@ -17,6 +17,28 @@ module.exports = (app, conn) => {
     
 
     // Route for store
+    app.route('/enterprises/notVL')
+    .get(controller.getEntNotInVL_Has_lt4_store)
+
+    app.route('/enterprises/:id/stores')
+    .get(controller.getAllStoreByEntID)
+
+    app.route('/enterprises/:id/stores/count')
+    .get(controller.getStoreCountByID)
+
+    app.route('/stores')
+    .get(controller.getAllStore)
+
+    app.route('/stores/not/:id')
+    .get(controller.getAllStoreAddressExcept)
     
+    app.route('/stocks')
+    .get(controller.getAllStock)
+
+    app.route('/stocks/mt2')
+    .get(controller.getStockSupporting_mt2_Store)
+
+    app.route('/stocks/area/:square')
+    .get(controller.getStockAreaLessThan)
     
 }
